55cdd90174829a799ba34cf3442a70f04ac5d35c
kartik <karthik.joshi103@gmail.com>
2023-02-17 11:34:13 +0530

    Updated the text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,59 +1,95 @@
-import React, { useState } from 'react'
-import './Description.scss'
-import profile from '../../Assets/profilepic.jpeg'
+import React, { useState } from "react";
+import "./Description.scss";
+import profile from "../../Assets/profilepic.jpeg";
 import { HiOutlinePlus, HiOutlineMinusSm } from "react-icons/hi";
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
 //
 function About() {
+  const [showansone, setshowansone] = useState(false);
+  const [showanstwo, setshowanstwo] = useState(false);
+  const [showansthree, setshowansthree] = useState(false);
 
-    const [showansone, setshowansone] = useState(false)
-    const [showanstwo, setshowanstwo] = useState(false)
-    const [showansthree, setshowansthree] = useState(false)
-
-
-    return (
-        <div className='container AboutContainer my-3'>
-            <div className='AccordianMain-Container'>
-                <h2 className='py-1'>What  to expect from my work</h2>
-                <div className="accordiandiv">
-                    <div className="accordianitem">
-                        <div className="questiondiv">
-                            <h3 className='question'>Development</h3>
-                            <button className='btn' onClick={() => setshowanstwo((previous) => !previous)}>{showanstwo ? <HiOutlineMinusSm color='#ff3f2f' fontSize={30} /> : <HiOutlinePlus color='#ff3f2f' fontSize={30} />}</button>
-                        </div>
-                        <div className='text-div'>
-                            {showanstwo && <p className='ans'>I Like to develop web based solutions to modern problems</p>}
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="accordianitem">
-                        <div className="questiondiv">
-                            <h3 className='question'>Design</h3>
-                            <button className='btn' onClick={() => setshowansone((previous) => !previous)}>
-                                {showansone ? <HiOutlineMinusSm color='#ff3f2f' fontSize={30} /> : <HiOutlinePlus color='#ff3f2f' fontSize={30} />}</button>
-                        </div>
-                        <div className='text-div'>
-                            {showansone && <p className='ans'>Designing and implenting pleasent UI is first Priority in Development</p>}
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="accordianitem">
-                        <div className="questiondiv">
-                            <h3 className='question'>Strategy</h3>
-                            <button className='btn' onClick={() => setshowansthree((previous) => !previous)}>
-                                {showansthree ? <HiOutlineMinusSm color='#ff3f2f' fontSize={30} /> : <HiOutlinePlus color='#ff3f2f' fontSize={30} />}
-                            </button>
-                        </div>
-                        <div className='text-div'>
-                            {showansthree && <p className='ans'>Software Engineering is all about solving particular problem And I always try to solve that effeciently</p>}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="container AboutContainer my-3">
+      <div className="AccordianMain-Container">
+        <h2 className="py-1">What To Expect From My Work</h2>
+        <div className="accordiandiv">
+          <div className="accordianitem">
+            <div className="questiondiv">
+              <h3 className="question">Development</h3>
+              <button
+                className="btn"
+                onClick={() => setshowanstwo((previous) => !previous)}
+              >
+                {showanstwo ? (
+                  <HiOutlineMinusSm color="#ff3f2f" fontSize={30} />
+                ) : (
+                  <HiOutlinePlus color="#ff3f2f" fontSize={30} />
+                )}
+              </button>
             </div>
-            <Tilt className='profile-div mx-1'>
-                <img src={profile} alt="profile" />
-            </Tilt>
+            <div className="text-div">
+              {showanstwo && (
+                <p className="ans">
+                  I Like To Develop Web Based Solutions To Modern Problems
+                </p>
+              )}
+            </div>
+          </div>
+          <hr />
+          <div className="accordianitem">
+            <div className="questiondiv">
+              <h3 className="question">Design</h3>
+              <button
+                className="btn"
+                onClick={() => setshowansone((previous) => !previous)}
+              >
+                {showansone ? (
+                  <HiOutlineMinusSm color="#ff3f2f" fontSize={30} />
+                ) : (
+                  <HiOutlinePlus color="#ff3f2f" fontSize={30} />
+                )}
+              </button>
+            </div>
+            <div className="text-div">
+              {showansone && (
+                <p className="ans">
+                  Designing and Implenting Pleasent UI Is First Priority In
+                  Development
+                </p>
+              )}
+            </div>
+          </div>
+          <hr />
+          <div className="accordianitem">
+            <div className="questiondiv">
+              <h3 className="question">Strategy</h3>
+              <button
+                className="btn"
+                onClick={() => setshowansthree((previous) => !previous)}
+              >
+                {showansthree ? (
+                  <HiOutlineMinusSm color="#ff3f2f" fontSize={30} />
+                ) : (
+                  <HiOutlinePlus color="#ff3f2f" fontSize={30} />
+                )}
+              </button>
+            </div>
+            <div className="text-div">
+              {showansthree && (
+                <p className="ans">
+                  Software Engineering Is All About Solving Particular Problem
+                  And I Always Try To Solve That Effeciently
+                </p>
+              )}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <Tilt className="profile-div mx-1">
+        <img src={profile} alt="profile" />
+      </Tilt>
+    </div>
+  );
 }
-export default React.memo(About)
+export default React.memo(About);
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,74 +1,74 @@
-import React from 'react'
-import './Experience.scss'
-import ScrollAnimation from 'react-animate-on-scroll';
+import React from "react";
+import "./Experience.scss";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default function Experience() {
-    return (
-        <div className='my-2 container Experience-Wrapper'>
-            <div className='Experience-Container'>
-                <hr size="830" width="6" id="verticalbar" />
-                <div className="grid-container">
-                    <div className="grid-item">
-                        <div className='emptyDiv'></div>
-                    </div>
-                    {/* 1-grid */}
-                    <div className="grid-item">
-                        <ScrollAnimation animateIn="fadeInRight" className='ExperienceDiv'>
-                            <div className='Content' id="project1">
-                                <h3>Lehjro Pvt </h3>
-                                <h5>FRONT-END DEVELOPER INTERN</h5>
-                                <p>JULY 2021 TO SEPTEMBER 2021</p>
-                            </div>
-                        </ScrollAnimation>
-                    </div>
-                    {/* 2-grid */}
-                    <div className="grid-item">
-                        <ScrollAnimation animateIn="fadeInLeft" className='ExperienceDiv'>
-                            <div className='Content' id="project2">
-                                <h3>CUROMATES</h3>
-                                <h5>WEB DEVELOPER INTERN</h5>
-                                <p>JAN 2022 TO FEB 2022</p>
-                            </div>
-                        </ScrollAnimation>
-                    </div>
-                    {/* 3-grid */}
-                    <div className="grid-item">
-                        <div className='middleitem'></div>
-                    </div>
-                    {/* 4-grid */}
-                    <div className="grid-item">
-                        <div className='emptyDiv'></div>
-                    </div>
-                    {/* 5-grid */}
-                    <div className="grid-item">
-                        <ScrollAnimation animateIn="fadeInRight" className='ExperienceDiv'>
-                            <div className='Content' id='project3'>
-                                <h3>PERSISTENT SYSTEMS</h3>
-                                <h5>SOFTWARE ENGINEER</h5>
-                                <p>FEB 2022 - CURRENT</p>
-                            </div>
-                        </ScrollAnimation>
-                    </div>
-                    {/* 6-grid */}
-                </div>
-            </div>
-            <ScrollAnimation animateIn="fadeIn" className="Mobile-container">
-                <div className="mobileContainerItem Content mx-2">
-                    <h3>Lehjro Pvt </h3>
-                    <h5>FRONT-END DEVELOPER INTERN</h5>
-                    <p>JULY 2021 TO SEPTEMBER 2021</p>
-                </div>
-                <div className="mobileContainerItem Content mx-2">
-                    <h3>CUROMATES</h3>
-                    <h5>WEB DEVELOPER INTERN</h5>
-                    <p>JAN 2022 TO FEB 2022</p>
-                </div>
-                <div className="mobileContainerItem Content mx-2">
-                    <h3>PERSISTENT SYSTEMS</h3>
-                    <h5>SOFTWARE ENGINEER</h5>
-                    <p>FEB 2022 - CURRENT</p>
-                </div>
+  return (
+    <div className="my-2 container Experience-Wrapper">
+      <div className="Experience-Container">
+        <hr size="830" width="6" id="verticalbar" />
+        <div className="grid-container">
+          <div className="grid-item">
+            <div className="emptyDiv"></div>
+          </div>
+          {/* 1-grid */}
+          <div className="grid-item">
+            <ScrollAnimation animateIn="fadeInRight" className="ExperienceDiv">
+              <div className="Content" id="project1">
+                <h3>LEHJRO PVT</h3>
+                <h5>FRONT-END DEVELOPER INTERN</h5>
+                <p>JULY 2021 TO SEPTEMBER 2021</p>
+              </div>
             </ScrollAnimation>
+          </div>
+          {/* 2-grid */}
+          <div className="grid-item">
+            <ScrollAnimation animateIn="fadeInLeft" className="ExperienceDiv">
+              <div className="Content" id="project2">
+                <h3>CUROMATES</h3>
+                <h5>WEB DEVELOPER INTERN</h5>
+                <p>JAN 2022 TO FEB 2022</p>
+              </div>
+            </ScrollAnimation>
+          </div>
+          {/* 3-grid */}
+          <div className="grid-item">
+            <div className="middleitem"></div>
+          </div>
+          {/* 4-grid */}
+          <div className="grid-item">
+            <div className="emptyDiv"></div>
+          </div>
+          {/* 5-grid */}
+          <div className="grid-item">
+            <ScrollAnimation animateIn="fadeInRight" className="ExperienceDiv">
+              <div className="Content" id="project3">
+                <h3>PERSISTENT SYSTEMS</h3>
+                <h5>SOFTWARE ENGINEER</h5>
+                <p>FEB 2022 - CURRENT</p>
+              </div>
+            </ScrollAnimation>
+          </div>
+          {/* 6-grid */}
         </div>
-    )
+      </div>
+      <ScrollAnimation animateIn="fadeIn" className="Mobile-container">
+        <div className="mobileContainerItem Content mx-2">
+          <h3>LEHJRO PVT </h3>
+          <h5>FRONT-END DEVELOPER INTERN</h5>
+          <p>JULY 2021 TO SEPTEMBER 2021</p>
+        </div>
+        <div className="mobileContainerItem Content mx-2">
+          <h3>CUROMATES</h3>
+          <h5>WEB DEVELOPER INTERN</h5>
+          <p>JAN 2022 TO FEB 2022</p>
+        </div>
+        <div className="mobileContainerItem Content mx-2">
+          <h3>PERSISTENT SYSTEMS</h3>
+          <h5>SOFTWARE ENGINEER</h5>
+          <p>FEB 2022 - CURRENT</p>
+        </div>
+      </ScrollAnimation>
+    </div>
+  );
 }
