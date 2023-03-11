a1cbbb31af733c300b52f3d47be8f2c27029a856
kartik <karthik.joshi103@gmail.com>
2023-03-11 14:48:04 +0530

    added project

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,50 +1,87 @@
-import React, { useState } from 'react'
-import './Nav.scss'
+import React, { useState } from "react";
+import "./Nav.scss";
 import { SiDevpost } from "react-icons/si";
-import resume from '../../Assets/Kartik_26_12_22.pdf'
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import ScrollAnimation from 'react-animate-on-scroll';
-
+import resume from "../../Assets/Kartik_11_3_23.pdf";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default function Nav() {
-    const [activeCls, setactiveCls] = useState(false)
-    function clickHandler() {
-        setactiveCls(!activeCls)
-    }
-    return (
-        <div>
-            <div className='container'>
-                <nav>
-                    <div className="leftnav">
-                        <SiDevpost color='#ff3f2f' id="devLogo" fontSize={45} />
-                        <h3>Kartik Joshi</h3>
-                    </div>
-                    <div className={`HamburgerIconDiv ${activeCls && 'bgSecondary'}`} onClick={clickHandler} >
-                        {activeCls ? <CloseIcon className="text-primary" /> : <MenuIcon className="text-primary" />}
-                    </div>
-                    <div className="rightnav">
-                        <ul>
-                            <li><a href="#SkillSection">Skills</a></li>
-                            <li><a href="#ProjectSection">Projects</a></li>
-                            <li><a href="#ExperienceSection">Experience</a></li>
-                            <li><a href="#FaqSection">FAQ</a></li>
-                            <li><a href="#ContactSection">Contact</a></li>
-                            <li><a href={resume} download="Resume">Resume</a></li>
-                        </ul>
-                    </div>
-                </nav>
-                <ScrollAnimation animateIn="fadeInRight" className={`HamburgerMenu ${activeCls ? "show SideFadeIn" : "hide"}`}>
-                    <ul>
-                        <li><a href="#SkillSection">Skills</a></li>
-                        <li><a href="#ProjectSection">Projects</a></li>
-                        <li><a href="#ExperienceSection">Experience</a></li>
-                        <li><a href="#FaqSection">FAQ</a></li>
-                        <li><a href="#ContactSection">Contact</a></li>
-                        <li><a href={resume} download="Resume">Resume</a></li>
-                    </ul>
-                </ScrollAnimation>
-            </div>
-        </div>
-    )
+  const [activeCls, setactiveCls] = useState(false);
+  function clickHandler() {
+    setactiveCls(!activeCls);
+  }
+  return (
+    <div>
+      <div className="container">
+        <nav>
+          <div className="leftnav">
+            <SiDevpost color="#ff3f2f" id="devLogo" fontSize={45} />
+            <h3>Kartik Joshi</h3>
+          </div>
+          <div
+            className={`HamburgerIconDiv ${activeCls && "bgSecondary"}`}
+            onClick={clickHandler}
+          >
+            {activeCls ? (
+              <CloseIcon className="text-primary" />
+            ) : (
+              <MenuIcon className="text-primary" />
+            )}
+          </div>
+          <div className="rightnav">
+            <ul>
+              <li>
+                <a href="#SkillSection">Skills</a>
+              </li>
+              <li>
+                <a href="#ProjectSection">Projects</a>
+              </li>
+              <li>
+                <a href="#ExperienceSection">Experience</a>
+              </li>
+              <li>
+                <a href="#FaqSection">FAQ</a>
+              </li>
+              <li>
+                <a href="#ContactSection">Contact</a>
+              </li>
+              <li>
+                <a href={resume} download="Resume">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <ScrollAnimation
+          animateIn="fadeInRight"
+          className={`HamburgerMenu ${activeCls ? "show SideFadeIn" : "hide"}`}
+        >
+          <ul>
+            <li>
+              <a href="#SkillSection">Skills</a>
+            </li>
+            <li>
+              <a href="#ProjectSection">Projects</a>
+            </li>
+            <li>
+              <a href="#ExperienceSection">Experience</a>
+            </li>
+            <li>
+              <a href="#FaqSection">FAQ</a>
+            </li>
+            <li>
+              <a href="#ContactSection">Contact</a>
+            </li>
+            <li>
+              <a href={resume} download="Resume">
+                Resume
+              </a>
+            </li>
+          </ul>
+        </ScrollAnimation>
+      </div>
+    </div>
+  );
 }
--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,63 +1,61 @@
-import React from 'react'
-import './Project.scss'
-import projimg1 from '../../Assets/project1.jpg'
-import projimg2 from '../../Assets/project2.jpg'
-import projimg3 from '../../Assets/project3.jpg'
-import projimg4 from '../../Assets/project4.png'
-import Projectitem from './Projectitem'
+import React from "react";
+import "./Project.scss";
+import projimg1 from "../../Assets/project1.jpg";
+import projimg2 from "../../Assets/project2.jpg";
+import projimg3 from "../../Assets/project3.jpg";
+import projimg4 from "../../Assets/project4.jpg";
+import Projectitem from "./Projectitem";
 export default function Project() {
-    const projects = [
-        {
-            projectName: "Linkedin Clone",
-            projectDesc: `User can perform operations on posts and comments ,can upload a photo`,
-            projectLink: 'https://web-linkedin.netlify.app/',
-            gitrepo: 'https://github.com/kdj309/Linkedin-clone',
-            alterstyle: false,
-            projectImg: projimg1
-        },
-        {
-            projectName: "Online Notes",
-            projectDesc: `Notes site where Users can perform store their notes and filter`,
-            projectLink: 'https://notessaver.netlify.app/',
-            gitrepo: 'https://github.com/kdj309/notescllient',
-            projectImg: projimg2,
-            alterstyle: false
-        },
-        {
-            projectName: "Weather Web App",
-            projectDesc: `web tool to gather weather information like week forecast and cities weather information`,
-            projectLink: 'https://weatherwikipedia.netlify.app/',
-            gitrepo: 'https://github.com/kdj309/weatherinfo',
-            alterstyle: false,
-            projectImg: projimg3
-        },
-        {
-            projectName: "Recipe Wikipedia",
-            projectDesc: `Recipe finder site where users can search, sort, filter different kinds of
-            recipe based on tags, etc`,
-            projectLink: 'https://indianfoodie.netlify.app/',
-            gitrepo: 'https://github.com/kdj309/recipe_web_app',
-            alterstyle: false,
-            projectImg: projimg4
-        },
-    ]
-    return (
-        <div className='ProjectMainContainer my-2'>
-            <div className='container Project-Section'>
-                {
-                    projects.map((proj) => (
-                        <Projectitem
-                            key={proj.projectName}
-                            projectTitle={proj.projectName}
-                            projectDescription={proj.projectDesc}
-                            projectLink={proj.projectLink}
-                            projectimg={proj.projectImg}
-                            alterstyle={proj.alterstyle}
-                            gitrepo={proj.gitrepo}
-                        />))
-                }
-            </div>
-            <div className='Project-Background'></div>
-        </div>
-    )
+  const projects = [
+    {
+      projectName: "Linkedin Clone",
+      projectDesc: `User can perform operations on posts and comments ,can upload a photo`,
+      projectLink: "https://web-linkedin.netlify.app/",
+      gitrepo: "https://github.com/kdj309/Linkedin-clone",
+      alterstyle: false,
+      projectImg: projimg1,
+    },
+    {
+      projectName: "Online Notes",
+      projectDesc: `Notes site where Users can perform store their notes and filter`,
+      projectLink: "https://notessaver.netlify.app/",
+      gitrepo: "https://github.com/kdj309/notescllient",
+      projectImg: projimg2,
+      alterstyle: false,
+    },
+    {
+      projectName: "Weather Web App",
+      projectDesc: `web tool to gather weather information like week forecast and cities weather information`,
+      projectLink: "https://weatherwikipedia.netlify.app/",
+      gitrepo: "https://github.com/kdj309/weatherinfo",
+      alterstyle: false,
+      projectImg: projimg3,
+    },
+    {
+      projectName: "E-Commerce Web App",
+      projectDesc: `The application offers a seamless and secure shopping experience with core functionalities`,
+      projectLink: "https://myntraclone-inky.vercel.app/",
+      gitrepo: "https://github.com/kdj309/e-commerce-frontend",
+      alterstyle: false,
+      projectImg: projimg4,
+    },
+  ];
+  return (
+    <div className="ProjectMainContainer my-2">
+      <div className="container Project-Section">
+        {projects.map((proj) => (
+          <Projectitem
+            key={proj.projectName}
+            projectTitle={proj.projectName}
+            projectDescription={proj.projectDesc}
+            projectLink={proj.projectLink}
+            projectimg={proj.projectImg}
+            alterstyle={proj.alterstyle}
+            gitrepo={proj.gitrepo}
+          />
+        ))}
+      </div>
+      <div className="Project-Background"></div>
+    </div>
+  );
 }
