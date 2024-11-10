e3585b16b990e575f3c629fa6db9b90939301020
Kartik Joshi <67888831+kdj309@users.noreply.github.com>
2024-11-10 13:17:37 +0530

    Update Projectitem.js

--- src/components/Projects/Projectitem.js
+++ src/components/Projects/Projectitem.js
@@ -4,16 +4,14 @@ import { AiFillEye } from "react-icons/ai";
 import { BiCodeAlt } from "react-icons/bi";
 import Tilt from 'react-parallax-tilt';
 
-export default function Projectitem({ projectimg, projectTitle, projectLink, projectDescription, gitrepo }) {
-
-
+export default function Projectitem({ projectimg, projectTitle, projectLink, projectDescription, gitrepo,isActive }) {
 
     return (
         <Tilt className="MaincardItem">
             <div className="card">
                 <img src={projectimg} className='card__image' alt='projectimg' />
                 <div className='btn-container'>
-                    <a href={projectLink} className='btn' id='Viewbtn'>View <AiFillEye /></a>
+                    {isActive&&<a href={projectLink} className='btn' id='Viewbtn'>View <AiFillEye /></a>}
                     <a href={gitrepo} className='btn' id='Codebtn'>Code <BiCodeAlt /></a>
                 </div>
             </div>
